4544db969220b55b4536d61763eca96e22b90dab
isma90 <ismaleiva90@gmail.com>
2019-06-03 11:23:38 -0400

    daemon to kill the process and restart again

--- utils/dashboards.sh
+++ utils/dashboards.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-kill -9 $(lsof -t -i:3000)
-kill -9 $(lsof -t -i:9090)
-rm -r k8s.log
-rm -r grafana.log
-
-kubectl port-forward --namespace monitoring service/grafana 3000:3000 > ./k8s.log &
-K8S=$(kubectl get pods -n kube-system | grep -oe "kubernetes-dashboard-[a-z0-9]*-[a-z0-9]*")
-kubectl port-forward ${K8S} 9090:9090 -n kube-system > ./grafana.log &
-tail -f k8s.log & tail -f grafana.log
+while true; do
+    kill -9 $(lsof -t -i:3000)
+    kill -9 $(lsof -t -i:9090)
+    sleep 2s
+    kubectl port-forward -n monitoring service/grafana 3000:3000 > ./k8s.log &
+    K8S=$(kubectl get pods -n kube-system | grep -oe "kubernetes-dashboard-[a-z0-9]*-[a-z0-9]*")
+    kubectl port-forward -n kube-system ${K8S} 9090:9090 > ./grafana.log &
+    sleep 60s
+done
